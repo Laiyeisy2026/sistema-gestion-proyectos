408967e105cd5729fc3acbe0d4e81a08b18b2e3d
Laiyeisy Quinche <laiyeisy@uniminuto.edu.co>
2026-02-11 19:04:10 -0500

    Remove create_all on startup

--- main.py
+++ main.py
@@ -51,10 +51,6 @@ supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
 app = FastAPI()
 
-@app.on_event("startup")
-def startup():
-    Base.metadata.create_all(bind=engine)
-
 # =========================
 # USUARIOS (LOGIN SIMPLE)
 # =========================
