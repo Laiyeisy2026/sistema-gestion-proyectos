330f5df1e9cf47ca08e8c7cef91dcf4e2104f649
Laiyeisy Quinche <laiyeisy@uniminuto.edu.co>
2026-02-11 19:51:16 -0500

    Fix session https

--- main.py
+++ main.py
@@ -103,7 +103,8 @@ os.makedirs("static/dashboards", exist_ok=True)
 app.add_middleware(
     SessionMiddleware,
     secret_key="clave-super-secreta-cambiala",
-    max_age=30 * 60  # 30 minutos
+    max_age=30 * 60,
+    https_only=True
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
