0bfeaa3bcd8dcab1d9c2d73391ac6f5b0ec539d8
Laiyeisy Quinche <laiyeisy@uniminuto.edu.co>
2026-02-11 20:17:55 -0500

    Fix Railway Postgres SSL connection

--- database.py
+++ database.py
@@ -10,7 +10,10 @@ if not DATABASE_URL:
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    connect_args={
+        "sslmode": "require"
+    }
 )
 
 SessionLocal = sessionmaker(
