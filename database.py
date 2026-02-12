30006f1622693b72739fc93c7b825d43e45a1320
Laiyeisy Quinche <laiyeisy@uniminuto.edu.co>
2026-02-11 19:42:07 -0500

    Fix railway connection pool

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,3 +20,5 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
